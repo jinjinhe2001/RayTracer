c8a21cee749bdd456ddd0bba732d2ba41c0367f1
Jinjin He <hust_hjj@126.com>
2022-09-18 18:29:09 -0700

    dielectric

--- main.cpp
+++ main.cpp
@@ -38,8 +38,8 @@ int main() {
 
     auto material_groud = make_shared<lambertian>(color(0.8, 0.8, 0.0));
     auto material_center = make_shared<lambertian>(color(0.5, 0.2, 1.0));
-    auto material_left = make_shared<metal>(color(0.4, 0.5, 0.4), 0.8);
-    auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 0.3);
+    auto material_left = make_shared<dielectric>(1.5);
+    auto material_right = make_shared<dielectric>(1.5);
 
     world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.5), 100.0, material_groud));
     world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.0),   0.5, material_center));
