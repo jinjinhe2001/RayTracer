0175c6b0534145eebb71f7e2eb9c45fe79e1cf83
Jinjin He <hust_hjj@126.com>
2022-09-18 21:39:55 -0700

    approximate dielectric

--- main.cpp
+++ main.cpp
@@ -37,12 +37,13 @@ int main() {
     hittable_list world;
 
     auto material_groud = make_shared<lambertian>(color(0.8, 0.8, 0.0));
-    auto material_center = make_shared<lambertian>(color(0.5, 0.2, 1.0));
-    auto material_left = make_shared<dielectric>(1.5);
-    auto material_right = make_shared<dielectric>(1.5);
+    auto material_center = make_shared<lambertian>(color(1.0, 0.2, 0.5));
+    auto material_left = make_shared<dielectric>(2.4);
+    auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
 
     world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.5), 100.0, material_groud));
     world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.0),   0.5, material_center));
+    world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   -0.4, material_left));
     world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));
     world.add(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
 
